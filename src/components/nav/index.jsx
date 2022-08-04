b1789b908adbbed95e9e04f3854868d01acffc80
KMalif <khafidulmualif2019@gmail.com>
2022-08-04 16:22:25 +0700

    create simple pokedex web

---
+++ src/components/nav/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Layout } from 'antd';
+const { Header } = Layout;
+
+const Navbar =()=>{
+    
+    return(
+        <>
+            <Header>
+                <div className="logo"></div>
+            </Header>
+
+        </>
+
+    )
+}
+
+export default Navbar
